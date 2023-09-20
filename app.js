3f0511a7b8ee612ef5f7f33e41d7200d41611593
Banu Güngör <banuuzun9@gmail.com>
2023-09-20 14:08:06 +0300

    app

--- app.js
+++ app.js
@@ -9,15 +9,19 @@ if (navToggle) {
   });
 }
 /*=============== MENU HIDDEN ===============*/
-if(navClose){
-    navClose.addEventListener('click',()=>{
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
-
 /*=============== REMOVE MENU MOBILE ===============*/
-
+const navLink = document.querySelectorAll(".nav__link");
+const linkAction = () => {
+  //   const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 /*=============== SHADOW HEADER ===============*/
 
 /*=============== EMAIL JS ===============*/
